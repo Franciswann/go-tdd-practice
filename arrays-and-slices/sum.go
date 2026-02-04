319e0620ae433b40bded9b17cf21ac0a07ff1276
francis <a0975568423@gmail.com>
2026-02-04 17:10:03 +0800

    feat: add SumAll function to sum multiple slices
    - Accept variadic parameters of []int
    - Return slice containing sum of each input slice
    - Reuse existing Sum function for calculation

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,9 +1,27 @@
 package main
 
+import "fmt"
+
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	// (0, 1)
+	// ([]int{1, 2}, []int{0, 9})
+	// i : 0, 1 | numbers := [1, 2], [0, 9]
+	for i, numbers := range numbersToSum {
+		fmt.Println(i)
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+
 }
